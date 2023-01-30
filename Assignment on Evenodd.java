cd85c8a3050d1c39a50de7ed17af4862d153b483
Moin Mulla <moinmulla555@gmail.com>
2023-01-30 14:47:12 +0530

    Java Assignment

---
+++ Assignment on Evenodd.java
@@ -0,0 +1,26 @@
+package com.javaapp;
+
+import java.util.Scanner;
+
+public class Evenodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number");
+		int num = sc.nextInt();
+		
+		
+		if(num%2==0) {
+			System.out.println("number is Even");
+			}
+		else {
+			System.out.println("number is Odd");
+		}
+		
+				
+	}
+
+}
